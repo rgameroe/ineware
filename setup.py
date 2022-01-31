56b6acc47c42cccde74160becb723e2333a143c3
roberto.gamero@worldline.com <roberto.gamero@worldline.com>
2022-01-31 20:18:17 +0100

    Modify setup.py with 0.0.3 version PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='ineware',
-    version='0.0.2',
+    version='0.0.3',
     description='A light weight Python library for INE API json-stat',
     long_description=long_description,
     long_description_content_type="text/markdown",
